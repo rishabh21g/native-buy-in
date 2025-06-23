19f5af2e5f5e33120235a9be60bf0d7af8a9e1cc
rishabh21g <biasedengineer@gmail.com>
2025-06-23 16:10:55 +0530

    data added

---
+++ data/data.js
@@ -0,0 +1,32 @@
+export const listItems = [
+  {
+    id: 0,
+    img: "https://media.istockphoto.com/id/1211554164/photo/3d-render-of-home-appliances-collection-set.jpg?s=612x612&w=0&k=20&c=blm3IyPyZo5ElWLOjI-hFMG-NrKQ0G76JpWGyNttF8s=",
+    name: "Home",
+  },
+  {
+    id: 1,
+    img: "https://img.global.news.samsung.com/ph/wp-content/uploads/2022/12/All-the-holiday-deals-and-freebies-from-Samsung-Digital-Appliances-this-Christmas_1-FOR-WEB-e1671095267726.png",
+    name: "Deals",
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/736x/b6/c9/d8/b6c9d886114f205f87457efdbeee4848.jpg",
+    name: "Electronics",
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXUVXERm3yWgAa9s5uAIG2et2PtD1mM-1n6g&s",
+    name: "Mobiles",
+  },
+  {
+    id: 4,
+    img: "https://nationalcomforts.com/cdn/shop/articles/2.webp?v=1718364044",
+    name: "Kitchen",
+  },
+  {
+    id: 5,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa0YsQdw-rnWgLef9U9YX731htKmlH-dzH6Q&s",
+    name: "Cosmetics",
+  },
+];
--- screens/Home.js
+++ screens/Home.js
@@ -7,23 +7,56 @@ import {
   ScrollView,
   Pressable,
   TextInput,
+  Image,
 } from "react-native";
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { listItems } from "../data/data";
 
 const Home = () => {
   return (
     <SafeAreaView style={styles.safeView}>
-      <View style={styles.searchContainer}>
-        <FontAwesome name="search" size={20} color="gray" />
-        <TextInput
-          style={styles.input}
-          placeholder="Search Amazon.in"
-          placeholderTextColor="gray"
-        />
-        <MaterialIcons name="mic" size={20} color="gray" />
-      </View>
-      <ScrollView></ScrollView>
+      <ScrollView>
+        {/* Search bar view */}
+        <View style={{ backgroundColor: "#f53d3d", paddingHorizontal: 5 }}>
+          <Pressable>
+            <View style={styles.searchContainer}>
+              <FontAwesome name="search" size={24} color="black" />
+              <TextInput
+                style={styles.input}
+                placeholder="Search Buy.in"
+                placeholderTextColor="gray"
+              />
+              <FontAwesome name="microphone" size={24} color="black" />
+            </View>
+          </Pressable>
+        </View>
+        {/* Search bar container ends */}
+        {/* Location View */}
+        <View style={styles.locationView}>
+          <FontAwesome6 name="location-dot" size={24} color="black" />
+          <Pressable>
+            <Text style={{ fontWeight: "500", fontSize: 12 }}>
+              Delivered to Rishabh Gupta , Saket, New Delhi 110030
+            </Text>
+          </Pressable>
+          <MaterialCommunityIcons name="menu-down" size={24} color="black" />
+        </View>
+        {/* Location view ends */}
+        {/* Scroll category items start horizontal scroll */}
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {listItems.map((item, idx) => {
+            return (
+              <Pressable key={item.id} style={styles.listItemContainer}>
+                <Image source={{ uri: item.img }} style={styles.image} />
+                <Text style={{ fontWeight: "600" }}>{item.name}</Text>
+              </Pressable>
+            );
+          })}
+        </ScrollView>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -36,20 +69,41 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "white",
   },
-   searchContainer: {
+  searchContainer: {
     flexDirection: "row",
     alignItems: "center",
-    backgroundColor: "white",
+    backgroundColor: "#F5F5F5",
     margin: 10,
     paddingHorizontal: 10,
-    borderWidth: 2,
-    borderColor: "#f53d3d",
-    borderRadius: 8,
+    borderRadius: 5,
     height: 40,
   },
   input: {
     flex: 1,
     paddingHorizontal: 10,
     color: "black",
+  },
+  locationView: {
+    backgroundColor: "#ffcccc",
+    height: 45,
+    color: "#808080",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    alignItems: "center",
+    gap: 5,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    resizeMode: "cover",
+    borderRadius: 8,
+  },
+  listItemContainer: {
+    flexDirection: "column",
+    gap: 5,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 15,
   },
 });
