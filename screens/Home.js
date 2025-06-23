09cc52a268251b799e32f98595633e4bcda3ee5d
rishabh21g <biasedengineer@gmail.com>
2025-06-23 14:46:59 +0530

    search bar complete

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,31 @@
-import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  ScrollView,
+  Pressable,
+  TextInput,
+} from "react-native";
 import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 const Home = () => {
-  return <SafeAreaView style={styles.safeView}></SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.safeView}>
+      <View style={styles.searchContainer}>
+        <FontAwesome name="search" size={20} color="gray" />
+        <TextInput
+          style={styles.input}
+          placeholder="Search Amazon.in"
+          placeholderTextColor="gray"
+        />
+        <MaterialIcons name="mic" size={20} color="gray" />
+      </View>
+      <ScrollView></ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
@@ -10,6 +33,23 @@ export default Home;
 const styles = StyleSheet.create({
   safeView: {
     paddingTop: Platform.OS === "android" ? 40 : 0,
-    flex:1
+    flex: 1,
+    backgroundColor: "white",
+  },
+   searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    margin: 10,
+    paddingHorizontal: 10,
+    borderWidth: 2,
+    borderColor: "#f53d3d",
+    borderRadius: 8,
+    height: 40,
+  },
+  input: {
+    flex: 1,
+    paddingHorizontal: 10,
+    color: "black",
   },
 });
