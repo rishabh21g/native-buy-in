2f8a1400c7e2b123df641ff27c0b526ca809cdb2
rishabh21g <biasedengineer@gmail.com>
2025-06-23 14:15:23 +0530

    home screen satrt

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+import React from "react";
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+  return <SafeAreaView style={styles.safeView}></SafeAreaView>;
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  safeView: {
+    paddingTop: Platform.OS === "android" ? 40 : 0,
+    flex:1
+  },
+});
